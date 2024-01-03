9fbebd5adb563f1b939451d1d9638ecb7e402355
Gani Ozturk <ganioztrk@gmail.com>
2024-01-03 19:58:38 +0300

    last

--- Slider/script.js
+++ Slider/script.js
@@ -1,4 +1,4 @@
-var models1 = [
+var models = [
     {
         name: "TOGG 1",
         image: "./img/1.jpeg"
@@ -14,13 +14,10 @@ var models1 = [
     {
         name: "TOGG 4",
         image: "./img/4.jpeg"
-    }
-];
-
-var models2 = [
+    },
     {
         name: "TOGG 5",
-        image: "./img/5.jpeg"
+        image: "./img/5.png"
     },
     {
         name: "TOGG 6",
@@ -40,24 +37,80 @@ var models2 = [
     }
 ];
 
+
 var index = 0;
+showSlide(index);
+var slaytCount = models.length;
+var interval;
 
-
-
-
-function indexArtır(){
-    index++;
-    console.log(index);
+var settings={
+    duration : '3000',
+    random : false
 };
-function indexAzalt(){
+
+init(settings);
+
+document.querySelector('.fa-arrow-circle-left').addEventListener('click',function(){
     index--;
-    console.log(index);
-};
+    showSlide(index);
+});
+
+document.querySelector('.fa-arrow-circle-right').addEventListener('click',function(){
+    index++;
+    showSlide(index);    
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseenter',function(){
+        clearInterval(interval);
+    })
+});
+
+document.querySelectorAll('.arrow').forEach(function(item){
+    item.addEventListener('mouseleave',function(){
+        init(settings);
+    })
+})
+
+
+function init(settings){
+
+    var prev;
+    interval=setInterval(function(){
+        
+        if(settings.random){
+            // random index
+            do{
+                index = Math.floor(Math.random() * slaytCount);
+            }while(index == prev)
+            prev = index;
+        }else{
+            // artan index
+            if(slaytCount == index+1){
+                index = -1;
+            }
+            showSlide(index);
+            index++;
+        }
+        showSlide(index);
+
+    },settings.duration)
+}
 
 
 
+function showSlide(i){
 
+    index = i;
 
-document.querySelector(".card-title").textContent = models1[index].name;
+    if (i<0) {
+        index = slaytCount - 1;
+    }
+    if(i >= slaytCount){
+        index =0;
+    }
 
-document.querySelector(".card-img-top").setAttribute("src", models1[index].image)
+    document.querySelector('.card-title').textContent = models[index].name;
+
+    document.querySelector('.card-img-top').setAttribute('src',models[index].image);
+}
