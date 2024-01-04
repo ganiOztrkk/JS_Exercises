4c6a4a60f3248660ef53c149dc74b49742f84863
Gani Ozturk <ganioztrk@gmail.com>
2024-01-04 22:58:14 +0300

    validator

---
+++ FormValidator/script.js
@@ -0,0 +1,40 @@
+const form = document.getElementById("form");
+const username = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const repassword = document.getElementById("repassword");
+
+form.addEventListener("submit", function(e){
+    e.preventDefault();
+    checkRequired([username, email, password, repassword]);
+});
+
+function checkRequired(inputs) {
+    inputs.forEach(function(input) {
+        if (input.value.trim() === "") {
+            error(input, "gerekli alan");
+        } else if (input === email && !validateEmail(input.value)) {
+            error(input, "email formatı yanlış");
+        } else {
+            success(input);
+        }
+    });
+}
+
+function error(input, message){
+    input.className = "form-control is-invalid"
+    const div = input.nextElementSibling;
+    div.innerText = message;
+    div.className = "invalid-feedback";
+}
+function success(input){
+    input.className ="form-control is-valid"
+}
+
+const validateEmail = (email) => {
+    return String(email)
+      .toLowerCase()
+      .match(
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
